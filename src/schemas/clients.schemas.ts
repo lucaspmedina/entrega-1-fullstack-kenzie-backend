4a91a83e7f78136c280025309c184a8ba9f2ea30
Lucas Medina <81653299+lucaspmedina@users.noreply.github.com>
2023-04-03 02:25:48 -0300

    create client service done

---
+++ src/schemas/clients.schemas.ts
@@ -0,0 +1,20 @@
+import * as yup from 'yup';
+import { IClients, IClientsRequest } from '../interfaces/clients';
+
+const clientsSchema: yup.SchemaOf<IClientsRequest> = yup.object().shape({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    phone: yup.string().required(),
+    password: yup.string().required(),
+});
+
+const clientsWhitNoPasswordSchema: yup.SchemaOf<IClients> = yup.object().shape({
+    id: yup.string().notRequired(),
+    name: yup.string().notRequired(),
+    email: yup.string().email().notRequired(),
+    phone: yup.string().notRequired(),
+    createdAt: yup.date().notRequired(),
+    updatedAt: yup.date().notRequired(),
+});
+
+export { clientsSchema, clientsWhitNoPasswordSchema };
---
+++ src/services/clients/createClient.service.ts
@@ -0,0 +1,42 @@
+import { hashSync } from 'bcryptjs';
+import { AppDataSource } from '../../data-source';
+import { Cliente } from '../../entities/clients/clients.entity';
+import { AppError } from '../../errors/app.errors';
+import { IClientsRequest } from '../../interfaces/clients';
+import { clientsWhitNoPasswordSchema } from '../../schemas/clients.schemas';
+
+const createNewClientService = async ({
+    name,
+    email,
+    phone,
+    password,
+}: IClientsRequest) => {
+    const clientsRepository = AppDataSource.getRepository(Cliente);
+
+    const emailAlreadyExists = await clientsRepository.findOneBy({ email });
+    if (emailAlreadyExists) {
+        throw new AppError('This email already exists in database', 400);
+    }
+
+    const checkIfPhoneExists = await clientsRepository.findOneBy({ phone });
+    if (checkIfPhoneExists) {
+        throw new AppError('This phone already exists in database', 400);
+    }
+
+    const newClient = new Cliente();
+    newClient.name = name;
+    newClient.email = email;
+    newClient.phone = phone;
+    newClient.password = hashSync(password, 12);
+
+    const createdNewClient = clientsRepository.create(newClient);
+    await clientsRepository.save(createdNewClient);
+
+    const returnedClient = await clientsWhitNoPasswordSchema.validate(
+        createdNewClient,
+        {
+            stripUnknown: true,
+        }
+    );
+    return returnedClient;
+};
